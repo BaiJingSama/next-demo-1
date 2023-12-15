067e00cc9183537503539d316a8650d324c05794
gyc <gouyuchen@zhiyuanhui.com>
2023-12-15 13:46:35 +0800

    init project

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          你好 <a href="https://4399.com">白境</a>
         </h1>
 
         <p className="description">
