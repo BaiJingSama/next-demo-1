b4d3fc9bb3e68ae7663f3f734f3d1acc82270d13
gyc <gouyuchen@zhiyuanhui.com>
2024-02-04 13:51:00 +0800

    使用 .env.local设置环境变量

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -1,9 +1,11 @@
 import { NextApiHandler } from 'next'
 import {withIronSession} from 'next-iron-session'
 
+console.log(process.env.SECRET,'环境变量');
+
 export function withSession(handler:NextApiHandler){
   return withIronSession(handler,{
-    // password: process.env.SECRET_COOKIE_PASSWORD, 加密的密钥
+    // password: process.env.SECRET, // 加密的密钥
     password: '50627CFA-0AA5-D53F-12C1-FE09D9802446', // 随机数 
     cookieName:'blog',
     cookieOptions: {
